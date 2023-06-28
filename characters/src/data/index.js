ff743be5e37f2f3913d358e793fcb437d211a37c
MakoskiVictor <makoski.ed@gmail.com>
2023-06-28 11:12:35 -0300

    export createdCharacter

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -3,5 +3,9 @@ const characters = require('./characters.json')
 module.exports = {
   list: async () => {
     return characters
+  },
+  created: async () => {
+    /* throw new Error({ error: Error.message }) */
+    return { message: 'Character created succesfully!' }
   }
 }
