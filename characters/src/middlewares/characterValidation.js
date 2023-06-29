978cc0126ea55ae7cb5a3fa710c5e880e180b869
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 10:58:58 -0300

    characterValidation midleware

---
+++ characters/src/middlewares/characterValidation.js
@@ -0,0 +1,7 @@
+const { ClientError } = require('../utils/errors')
+
+module.exports = (req, _res, next) => {
+  const { name } = req.body
+  if (name) return next()
+  else throw new ClientError('Name is missing', 401)
+}
