190c7f93f4ea55dc4283b9d00427d469e814ad62
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 10:59:36 -0300

    added errorHandler

--- characters/src/server.js
+++ characters/src/server.js
@@ -7,4 +7,12 @@ server.use(express.json())
 server.use(morgan('dev'))
 server.use(require('./routes'))
 
+server.use('*', (_req, res) => {
+  res.status(404).send('Not found')
+})
+// Error Handler
+server.use((err, req, res, next) => {
+  res.status(err.statusCode).send(err.message)
+})
+
 module.exports = server
