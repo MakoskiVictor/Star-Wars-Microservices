fc71e7b8f9abb4daa912c6cad61a20a8620748ad
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 12:23:03 -0300

    export crete Planet controller

--- planets/src/controllers/index.js
+++ planets/src/controllers/index.js
@@ -1,5 +1,6 @@
 const { catchAsync } = require('../utils')
 
 module.exports = {
-  getPlanets: catchAsync(require('./getPlanets'))
+  getPlanets: catchAsync(require('./getPlanets')),
+  createPlanet: catchAsync(require('./createPlanet'))
 }
