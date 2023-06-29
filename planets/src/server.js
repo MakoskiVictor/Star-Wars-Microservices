62ed9c69a9df63ab0c45855d4aebb181c3de3fd0
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 12:30:42 -0300

    fixed errorHandler

--- planets/src/server.js
+++ planets/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+
 const server = express()
 
 server.use(morgan('dev'))
@@ -8,6 +9,13 @@ server.use(require('./routes'))
 
 server.use('*', (_req, res) => {
   res.status(404).send('Not Found')
+})
+
+server.use((ClientError, req, res, next) => {
+  res.status(ClientError.statusCode).send({
+    error: true,
+    message: ClientError.message
+  })
 })
 
 module.exports = server
