513fd7ae9f0686a8ca5af5c7151b55c0dbed3974
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 12:09:12 -0300

    created post route

--- films/src/routes/index.js
+++ films/src/routes/index.js
@@ -1,8 +1,11 @@
 const controllers = require('../controllers')
+const midlewares = require('../midlewares')
 
 const { Router } = require('express')
 const router = Router()
 
 router.get('/', controllers.getFilms)
+
+router.post('/', midlewares.filmsValidation, controllers.postFilm)
 
 module.exports = router
