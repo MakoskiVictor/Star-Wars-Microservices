edc95be8caab7a34638b8a71886c351885f650a7
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 12:21:44 -0300

    created planet validation midleware

---
+++ planets/src/controllers/createPlanet.js
@@ -0,0 +1,7 @@
+const response = require('../utils')
+const Planet = require('../data')
+
+module.exports = async (req, res) => {
+  const planet = await Planet.create()
+  response(res, 201, planet)
+}
