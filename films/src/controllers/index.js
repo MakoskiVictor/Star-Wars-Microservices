1ba82b90309e4300d161ef88738ad5161fc23066
MakoskiVictor <makoski.ed@gmail.com>
2023-06-29 12:08:14 -0300

    export post controller

--- films/src/controllers/index.js
+++ films/src/controllers/index.js
@@ -1,5 +1,6 @@
 const { catchAsync } = require('../utils')
 
 module.exports = {
-  getFilms: catchAsync(require('./getFilms'))
+  getFilms: catchAsync(require('./getFilms')),
+  postFilm: catchAsync(require('./postFilm'))
 }
