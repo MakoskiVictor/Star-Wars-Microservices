be70dc8114e7752a716a4009bb48c4a2ea1bae31
MakoskiVictor <makoski.ed@gmail.com>
2023-06-30 12:16:08 -0300

    fixed route name

--- characters/src/routes/index.js
+++ characters/src/routes/index.js
@@ -3,8 +3,8 @@ const router = Router()
 const controllers = require('../controllers')
 const middlewares = require('../middlewares')
 
-router.get('/', controllers.getCharacters)
+router.get('/characters', controllers.getCharacters)
 
-router.post('/', middlewares.characterValidation, controllers.createCharacter)
+router.post('/characters', middlewares.characterValidation, controllers.createCharacter)
 
 module.exports = router
