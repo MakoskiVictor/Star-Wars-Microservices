de3ec4558c1e6145c4757b8e2092e00ca321eda8
MakoskiVictor <makoski.ed@gmail.com>
2023-06-30 12:16:12 -0300

    fixed route name

--- planets/src/routes/index.js
+++ planets/src/routes/index.js
@@ -4,8 +4,8 @@ const midlewares = require('../midlewares')
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', controllers.getPlanets)
+router.get('/planets', controllers.getPlanets)
 
-router.post('/', midlewares.planetValidation, controllers.createPlanet)
+router.post('/planets', midlewares.planetValidation, controllers.createPlanet)
 
 module.exports = router
