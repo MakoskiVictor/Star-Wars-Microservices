0a90f418107af6f2f5760697384b1761f899d9df
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 13:28:10 -0300

    created post item controller

---
+++ database/src/controllers/postItem.js
@@ -0,0 +1,9 @@
+const { response } = require('../utils')
+const store = require('../database')
+
+module.exports = async (req, res) => {
+  const { model } = req.params
+  const form = req.body
+  const data = await store[model].create(form)
+  response(res, 201, data)
+}
