05f902399dd160354dd2618cf4fc493359b22191
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 14:20:30 -0300

    use env

--- films/index.js
+++ films/index.js
@@ -1,5 +1,5 @@
 const server = require('./src/server')
-const { PORT } = require('./config/envs')
+const { PORT } = require('./src/config/envs')
 
 server.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`)
