4cacf396c0914e783baa6c9c360c650688d6a634
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 14:34:11 -0300

    created envs

---
+++ gateway/config/envs.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+module.exports = {
+  PORT: process.env.PORT,
+  CHARACTER_PORT: process.env.CHARACTER_PORT,
+  FILM_PORT: process.env.CHARACTER_PORT,
+  PLANET_PORT: process.env.PLANET_PORT
+}
