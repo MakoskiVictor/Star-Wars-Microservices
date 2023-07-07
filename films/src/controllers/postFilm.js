2a8a4bef27b76c2c1cdcb23a2e002fe99c9996c6
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 14:20:16 -0300

    change route

--- films/src/controllers/postFilm.js
+++ films/src/controllers/postFilm.js
@@ -1,7 +1,8 @@
-const Films = require('../data')
+const { URL_DATABASE } = require('../config/envs')
 const { response } = require('../utils')
 
-module.exports = async (_req, res) => {
-  const films = await Films.create()
+module.exports = async (req, res) => {
+  const data = req.body
+  const films = await fetch(`${URL_DATABASE}/Film`, data)
   response(res, 201, films)
 }
