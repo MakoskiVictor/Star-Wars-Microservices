db2b5150324dc214557b88c5db6a802eb7b7b00e
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 13:29:44 -0300

    used controlers

--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
-const store = require('../database')
 const { validateModel } = require('../middlewares')
+const { getList, getById, postItem } = require('../controllers')
 
 const router = Router()
 
-router.get('/:model', validateModel, async (req, res) => {
-  const { model } = req.params
-  const response = await store[model].list()
-  res.status(200).send(response)
-})
+router.get('/:model', validateModel, getList)
+
+router.get('/:model/:id', validateModel, getById)
+
+router.post('/:model', validateModel, postItem) // POST NO PROBADO
 
 module.exports = router
