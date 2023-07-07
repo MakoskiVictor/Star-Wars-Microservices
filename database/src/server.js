f6862d860038cf281f57c39956e6e4c3134f6de5
MakoskiVictor <makoski.ed@gmail.com>
2023-07-07 12:34:28 -0300

    created error handler

--- database/src/server.js
+++ database/src/server.js
@@ -6,4 +6,17 @@ const server = express()
 server.use(express.json())
 server.use(morgan('dev'))
 
+server.use(require('./routes'))
+
+server.use('*', (_req, res) => {
+  res.status(404).send('Not found')
+})
+// Error Handler
+server.use((ClientError, req, res, next) => {
+  res.status(ClientError.statusCode).send({
+    error: true,
+    message: ClientError.message
+  })
+})
+
 module.exports = server
