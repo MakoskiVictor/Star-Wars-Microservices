57950eb64d30cdd420b26cc4deb814cad4fb8304
MakoskiVictor <makoski.ed@gmail.com>
2023-07-10 17:34:47 -0300

    added .then

--- films/src/controllers/getFilms.js
+++ films/src/controllers/getFilms.js
@@ -2,6 +2,7 @@ const { URL_DATABASE } = require('../config/envs')
 const { response } = require('../utils')
 
 module.exports = async (_req, res) => {
-  const films = await fetch(`${URL_DATABASE}/Film`)
-  response(res, 200, films)
+  await fetch(`${URL_DATABASE}Film`)
+    .then(response => response.json())
+    .then(data => response(res, 200, data))
 }
