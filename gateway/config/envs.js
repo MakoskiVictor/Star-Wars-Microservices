34bd5e729683b673451ba7c90609ba9f0341a205
MakoskiVictor <makoski.ed@gmail.com>
2023-07-10 18:21:58 -0300

    fixend port

--- gateway/config/envs.js
+++ gateway/config/envs.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT,
   CHARACTER_PORT: process.env.CHARACTER_PORT,
-  FILM_PORT: process.env.CHARACTER_PORT,
+  FILM_PORT: process.env.FILM_PORT,
   PLANET_PORT: process.env.PLANET_PORT
 }
