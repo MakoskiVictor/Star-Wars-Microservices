6d840ce80956e06d76c24a4cbacb136e143d2083
MakoskiVictor <makoski.ed@gmail.com>
2023-07-11 09:17:11 -0300

    use axios to get

--- planets/src/controllers/getPlanets.js
+++ planets/src/controllers/getPlanets.js
@@ -1,7 +1,9 @@
-const Planets = require('../data')
 const { response } = require('../utils')
+const axios = require('axios')
+const { URL_DATABASE } = require('../config/envs')
 
 module.exports = async (_req, res) => {
-  const planets = await Planets.list()
+  const search = await axios.get(`${URL_DATABASE}/Planet`)
+  const planets = search.data
   response(res, 200, planets)
 }
