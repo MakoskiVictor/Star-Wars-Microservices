5b6c319e5452fd187c01d587bfaacdcb458b75a8
MakoskiVictor <makoski.ed@gmail.com>
2023-07-11 09:16:04 -0300

    use dotenv for port

--- characters/index.js
+++ characters/index.js
@@ -1,6 +1,5 @@
 const server = require('./src/server')
-
-const PORT = 8001
+const { PORT } = require('./src/config/envs')
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
