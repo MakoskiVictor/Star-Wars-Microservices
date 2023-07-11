56896ca93725c7f6eb5a100ed6ce5a00f32ed949
MakoskiVictor <makoski.ed@gmail.com>
2023-07-11 10:48:26 -0300

    erased console.logs

--- database/src/controllers/getById.js
+++ database/src/controllers/getById.js
@@ -2,7 +2,6 @@ const { response } = require('../utils')
 const store = require('../database')
 
 module.exports = async (req, res) => {
-  console.log('Leggó a la funcion')
   const { model, id } = req.params
   const data = await store[model].getById(id)
   response(res, 200, data)
