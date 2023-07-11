3625987be7b0f19ec654ca4702baa08dd58a1bfc
MakoskiVictor <makoski.ed@gmail.com>
2023-07-11 11:06:41 -0300

    created route get by id

--- films/src/routes/index.js
+++ films/src/routes/index.js
@@ -6,6 +6,8 @@ const router = Router()
 
 router.get('/films', controllers.getFilms)
 
+router.get('/films/:id', midlewares.idValidator, controllers.getFilmById)
+
 router.post('/films', midlewares.filmsValidation, controllers.postFilm)
 
 module.exports = router
