84ad0aa2c6505376414f25cb2fcc01f2f583b9e9
MakoskiVictor <makoski.ed@gmail.com>
2023-07-11 10:48:41 -0300

    change route to database

--- characters/src/controllers/getCharacterById.js
+++ characters/src/controllers/getCharacterById.js
@@ -3,8 +3,8 @@ const { URL_DATABASE } = require('../config/envs')
 const axios = require('axios')
 
 module.exports = async (req, res) => {
-  const id = req.params
-  const searchByID = await axios.get(`${URL_DATABASE}/Character/:${id}`)
+  const { id } = req.params
+  const searchByID = await axios.get(`${URL_DATABASE}/Character/${id}`)
   const character = searchByID.data
   response(res, 200, character)
 }
