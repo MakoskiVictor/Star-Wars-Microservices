799290e8e87c6368bbbef3e60f33ef56ec9ea7ec
MakoskiVictor <makoski.ed@gmail.com>
2023-07-13 10:28:33 -0300

    add validations to post

--- characters/src/middlewares/characterValidation.js
+++ characters/src/middlewares/characterValidation.js
@@ -1,7 +1,19 @@
+/* eslint-disable camelcase */
 const { ClientError } = require('../utils/errors')
 
 module.exports = (req, _res, next) => {
-  const { name } = req.body
-  if (name) return next()
+  const { name, image, description, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld, films } = req.body
+  if (name &&
+    image &&
+    description &&
+    height &&
+    mass &&
+    hair_color &&
+    skin_color &&
+    eye_color &&
+    birth_year &&
+    gender &&
+    homeworld &&
+    films) return next()
   else throw new ClientError('Name is missing', 401)
 }
