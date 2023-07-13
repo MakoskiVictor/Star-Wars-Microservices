a8c9508c6d91348db76d08bc467f90b5f4cd5fc9
MakoskiVictor <makoski.ed@gmail.com>
2023-07-13 10:39:46 -0300

    fix post character

--- characters/src/controllers/createCharacter.js
+++ characters/src/controllers/createCharacter.js
@@ -1,7 +1,9 @@
 const { response } = require('../utils')
-const Characters = require('../data')
-
-module.exports = async (_req, res) => {
-  const character = await Characters.created()
+const { URL_DATABASE } = require('../config/envs')
+const axios = require('axios')
+// Falta probar
+module.exports = async (req, res) => {
+  const form = req.body
+  const character = await axios.get(`${URL_DATABASE}/Character`, form)
   response(res, 201, character)
 }
