0fdbd3823a056ab3e2ed5d085ebfed08c0088f59
MakoskiVictor <makoski.ed@gmail.com>
2023-07-14 18:17:10 -0300

    add fields to validate

--- planets/src/midlewares/planetValidation.js
+++ planets/src/midlewares/planetValidation.js
@@ -1,7 +1,20 @@
+/* eslint-disable camelcase */
 const { ClientError } = require('../utils/errors')
 
 module.exports = (req, res, next) => {
-  const { name } = req.body
-  if (name) return next()
+  const { name, image, description, rotation_period, orbital_period, diameter, climate, gravity, terrain, surface_water, res_idents, films } = req.body
+  if (name &&
+      image &&
+      description &&
+      rotation_period &&
+      orbital_period &&
+      diameter &&
+      climate &&
+      gravity &&
+      terrain &&
+      surface_water &&
+      res_idents &&
+     films
+  ) return next()
   else throw new ClientError('Name is missing', 401)
 }
