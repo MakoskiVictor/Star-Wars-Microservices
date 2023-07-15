37e23334b6f7cff312736b1e39bbb12905f29e5d
MakoskiVictor <makoski.ed@gmail.com>
2023-07-15 04:19:44 -0300

    configure open cors

--- gateway/index.js
+++ gateway/index.js
@@ -1,28 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const { PLANET_PORT, CHARACTER_PORT, FILM_PORT, PORT } = require('./config/envs')
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 
 const app = express()
+
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200
+}
+
 app.use(morgan('dev'))
 
-app.get('./', (req, res) => {
-  res.status(200).send({
-    getAllCharacters: '/characters',
-    getAllPlanets: '/planets',
-    getAllFilms: '/films',
-    getCharacterById: '/characters/id',
-    getPlanetById: '/planets/id',
-    getFilmById: '/films/id',
-    postCharacter: '/characters',
-    postPlanet: '/planets',
-    postFilm: '/films',
-    deleteCharacter: '/characters/id',
-    deletePlanet: '/planets/id',
-    deleteFilm: '/planets/id'
-  })
-})
+
+app.use(cors(corsOptions))
 
 app.use('/characters', createProxyMiddleware({
   target: CHARACTER_PORT,
@@ -38,6 +31,23 @@ app.use('/planets', createProxyMiddleware({
   target: PLANET_PORT,
   changeOrigin: true
 }))
+
+app.get('/', (_req, res) => {
+  res.status(200).send({
+    getAllCharacters: '/characters',
+    getAllPlanets: '/planets',
+    getAllFilms: '/films',
+    getCharacterById: '/characters/id',
+    getPlanetById: '/planets/id',
+    getFilmById: '/films/id',
+    postCharacter: '/characters',
+    postPlanet: '/planets',
+    postFilm: '/films',
+    deleteCharacter: '/characters/id',
+    deletePlanet: '/planets/id',
+    deleteFilm: '/planets/id'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Gateway listen on port ${PORT}`)
