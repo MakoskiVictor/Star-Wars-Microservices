6855d41d7639b35e86fb2ca987a94ee36f80d1b2
MakoskiVictor <makoski.ed@gmail.com>
2023-07-15 04:14:27 -0300

    add directory

--- gateway/index.js
+++ gateway/index.js
@@ -7,6 +7,23 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 const app = express()
 app.use(morgan('dev'))
 
+app.get('./', (req, res) => {
+  res.status(200).send({
+    getAllCharacters: '/characters',
+    getAllPlanets: '/planets',
+    getAllFilms: '/films',
+    getCharacterById: '/characters/id',
+    getPlanetById: '/planets/id',
+    getFilmById: '/films/id',
+    postCharacter: '/characters',
+    postPlanet: '/planets',
+    postFilm: '/films',
+    deleteCharacter: '/characters/id',
+    deletePlanet: '/planets/id',
+    deleteFilm: '/planets/id'
+  })
+})
+
 app.use('/characters', createProxyMiddleware({
   target: CHARACTER_PORT,
   changeOrigin: true
