d9a135b572062b928f4849d4940bb14bc96e43a1
MakoskiVictor <makoski.ed@gmail.com>
2023-07-18 19:10:03 -0300

    erase client with next

--- client/components/Navigator/Navigator.jsx
+++
@@ -1,9 +0,0 @@
-const { default: Link } = require('next/link')
-
-export default function Navigator ({ pathNames }) {
-  return (
-    <div>
-      {pathNames.map((pathName) => <Link key={pathName.name} href={pathName.path}>{pathName.name}</Link>)}
-    </div>
-  )
-}
