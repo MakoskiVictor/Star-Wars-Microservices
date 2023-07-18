746f892dc7c0f67d6b42c74a16ec190d1f6ec77a
MakoskiVictor <makoski.ed@gmail.com>
2023-07-18 12:40:52 -0300

    create routes models

---
+++ client/models/routes.model.js
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  HOME: {
+    path: '/',
+    name: 'Home'
+  },
+  CHARACTERS: {
+    path: '/characters',
+    name: 'Characters'
+  },
+  FILMS: {
+    path: '/films',
+    name: 'Films'
+  },
+  PLANETS: {
+    path: '/planets',
+    name: 'Planets'
+  }
+}
