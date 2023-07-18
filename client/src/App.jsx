699f096a75656e32c1554f8092abf279716908b6
MakoskiVictor <makoski.ed@gmail.com>
2023-07-18 20:13:30 -0300

    creat routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import Landing from './pages/Landing/landing'
+import Home from './pages/Home/home'
+import Characteres from './pages/Characters/characters'
+import Films from './pages/Films/films'
+import Planets from './pages/Planets/planets'
 
 function App () {
   return (
-    <>
-     <h1>Star Wars</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+
+        <Route path='/' element={<Landing/>} />
+
+        <Route path='/home' element={<Home/>} />
+
+        <Route path='/characters' element={<Characteres/>} />
+
+        <Route path='/films' element={<Films/>} />
+
+        <Route path='/planets' element={<Planets/>} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
