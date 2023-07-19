fdeedbc5c1eb0d2e17e2827f56a42e97ea89715f
MakoskiVictor <makoski.ed@gmail.com>
2023-07-19 18:57:01 -0300

    implemente hooks useGetFilms

--- client/src/pages/Films/films.jsx
+++ client/src/pages/Films/films.jsx
@@ -1,5 +1,17 @@
+import { useGetFilms } from './hooks/useGetFilms'
+
 export default function Films () {
+  const { data, isLoading } = useGetFilms()
+  if (isLoading) return <div>Loading...</div>
   return (
-    <h2>Films</h2>
+    <>
+      <h2>Films</h2>
+      { data?.map(films => (
+        <div key={films._id} >
+          <p>{films.title} </p>
+          <img src={films.image} alt="Movie image" />
+        </div>
+      )) }
+    </>
   )
 }
