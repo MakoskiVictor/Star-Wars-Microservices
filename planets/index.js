bdb5a49b108ec69317a137d450f3186f719ffb89
MakoskiVictor <makoski.ed@gmail.com>
2023-07-19 12:21:06 -0300

    quit unnecessary envs

--- planets/index.js
+++ planets/index.js
@@ -1,5 +1,7 @@
 const server = require('./src/server')
-const { PORT } = require('./src/config/envs')
+// const { PORT } = require('./src/config/envs')
+
+const PORT = 8003
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- planets/src/config/envs.js
+++ planets/src/config/envs.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 module.exports = {
   PORT: process.env.PORT,
-  URL_DATABASE: process.env.URL_DATABASE
+  URL_DATABASE: 'http://database:8004'
 }
