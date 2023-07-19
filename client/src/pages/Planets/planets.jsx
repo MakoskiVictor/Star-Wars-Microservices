7a032c22558555ba2b6aefc2939abc1e27760609
MakoskiVictor <makoski.ed@gmail.com>
2023-07-19 19:15:09 -0300

    implemente hook useGetPlanets

--- client/src/pages/Planets/planets.jsx
+++ client/src/pages/Planets/planets.jsx
@@ -1,5 +1,19 @@
+import { useGetPlanets } from './hooks'
+
 export default function Planets () {
+  const { data, isLoading } = useGetPlanets()
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <h2>Planets</h2>
+    <>
+      <h2>Planets</h2>
+      { data?.map(planets => (
+        <div key={planets._id} >
+          <p>{planets.name} </p>
+          <img src={planets.image} alt="Planet image" />
+        </div>
+      )) }
+    </>
   )
 }
