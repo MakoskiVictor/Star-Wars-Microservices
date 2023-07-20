5a0fdca1892ceb84d9fef17e2792330131c5a349
MakoskiVictor <makoski.ed@gmail.com>
2023-07-20 10:47:27 -0300

    created not found page

---
+++ client/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import darthVader404 from '@/assets/NotFound/darthVader404.gif'
+
+export default function NotFound () {
+  return (
+    <div>
+      <img src={darthVader404} alt="Page Not Found" />
+    </div>
+  )
+}
