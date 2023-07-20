af30eb0b90ab865ad1b2e94fe53299961b1f2ed8
MakoskiVictor <makoski.ed@gmail.com>
2023-07-20 09:42:30 -0300

    extract ternary into constants

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,12 +1,15 @@
 import styles from './Card.module.css'
 
 export default function Card ({ props }) {
-  const name = props.name ?? props.title
-  return (
-    <div className={styles.card} >
-      <p className={styles.title} >{name}</p>
+  const filteredName = props.name ?? props.title
+  const filteredCardStyles = props.name === undefined ? styles.card2 : styles.card
+  const filteredImgStyles = props.name === undefined ? styles.img2 : styles.img
 
-        <img src={props.image} alt="Image" className={styles.img} />
+  return (
+    <div className={ filteredCardStyles } >
+      <p className={styles.title} >{filteredName}</p>
+
+        <img src={props.image} alt="Image" className={filteredImgStyles } />
 
     </div>
   )
