334aaccb3c8da8e69b8ff40d40a49148543f82f5
MakoskiVictor <makoski.ed@gmail.com>
2023-07-20 10:04:50 -0300

    use Loader component

--- client/src/pages/Landing/landing.jsx
+++ client/src/pages/Landing/landing.jsx
@@ -1,5 +1,10 @@
+import { Loader } from '../../components/Loader'
+
 export default function Landing () {
   return (
-    <h1>Landing Page</h1>
+    <>
+      <h1>Landing Page</h1>
+      <Loader/>
+    </>
   )
 }
