74896eeb61a3f9ce1f13ba90dc3c013887e11a41
MakoskiVictor <makoski.ed@gmail.com>
2023-07-20 10:04:31 -0300

    use Loader component

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import LoadingGear from '@/assets/Loaders/LoadingGear.svg'
+
+export default function Loader () {
+  return (
+    <div>
+      <img src={LoadingGear} alt="Loading" />
+    </div>
+  )
+}
