83ac01e10d67971424f56c0442d0e97ee8815c7d
MakoskiVictor <makoski.ed@gmail.com>
2023-08-09 18:57:35 -0300

    add useQuery

--- client/src/pages/DetailsCharacter/index.jsx
+++ client/src/pages/DetailsCharacter/index.jsx
@@ -1,35 +1,26 @@
 import styles from './DetailsCharacter.module.css'
 import CardDetails from '../../components/CardDetails'
-import { useEffect, useState } from 'react'
-import { BASE_URL } from '../../utils/constants'
 import { Loader } from '../../components/Loader'
 import { useParams } from 'react-router-dom'
+import { useGetCharacterId } from './hooks/useGetCharacterId'
+import { useQuery } from '@tanstack/react-query'
 
 export default function DetailsCharacter () {
   const { id } = useParams()
 
-  const [character, setCharacter] = useState()
-  /* const [error, setError] = useState(null) */
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    /* const abortController = new AbortController() */
-    setLoading(true)
-    fetch(`${BASE_URL}characters/${id}` /* { signal: abortController.signal } */)
-      .then(res => res.json())
-      .then(response => setCharacter(response.data))
-      .catch(error => console.log(error))
-      .finally(setLoading(false))
+  const { isLoading, data, error } = useQuery({
+    queryKey: ['character'],
+    queryFn: useGetCharacterId(id)
+  })
 
-    /* return () => abortController.abort() */
-  }, [id])
-  console.log('IM CHARACTER', character)
+  if (isLoading) return <Loader/>
+  if (error) return <p>Error: {error.message}</p>
+  console.log('CHARACTERS ==>', data)
+  console.log('ERROR ==>', error)
   return (
     <div className={styles.details} >
       {
-        loading && character !== undefined
-          ? <Loader />
-          : <CardDetails props={character} />
-
+       <CardDetails props={data} />
       }
 
     </div>
