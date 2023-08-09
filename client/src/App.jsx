b1f0276334734a71d71f4ab9658aefd94fcac9b3
MakoskiVictor <makoski.ed@gmail.com>
2023-08-09 18:55:53 -0300

    comment unused routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, BrowserRouter, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
 import Characters from './pages/Characters'
-import Films from './pages/Films'
-import Planets from './pages/Planets'
+/* import Films from './pages/Films'
+import Planets from './pages/Planets' */
 import NotFound from './pages/NotFound'
 import DetailsCharacter from './pages/DetailsCharacter'
-import DetailsFilm from './pages/DetailsFilm'
-import DetailsPlanet from './pages/DetailsPlanet'
+/* import DetailsFilm from './pages/DetailsFilm'
+import DetailsPlanet from './pages/DetailsPlanet' */
 
 function App () {
   return (
@@ -23,13 +23,13 @@ function App () {
 
         <Route path='/characters/:id' element={<DetailsCharacter/>} />
 
-        <Route path='/films' element={<Films/>} />
+        {/* <Route path='/films' element={<Films/>} />
 
         <Route path='/films/:id' element={<DetailsFilm/>} />
 
         <Route path='/planets' element={<Planets/>} />
 
-        <Route path='/planets/:id' element={<DetailsPlanet/>} />
+        <Route path='/planets/:id' element={<DetailsPlanet/>} /> */}
 
         <Route path='*' element={<NotFound/>} />
 
