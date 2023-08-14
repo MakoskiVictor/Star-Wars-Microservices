211968fa76129acd082b37a6cf9889d791c04797
MakoskiVictor <makoski.ed@gmail.com>
2023-08-14 20:01:28 -0300

    extract video into a component

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,19 +1,10 @@
-import YouTube from 'react-youtube'
+import { Video } from '../../components/Video'
 
 export default function Landing () {
-  const handleOnReady = (event) => {
-    event.target.playVideo() // Reproduce automáticamente el video
-  }
-
-  const opts = {
-    height: '780',
-    width: '1280'
-  }
-
   return (
     <>
       <h1>Landing Page</h1>
-      <YouTube videoId='5UnjrG_N8hU' opts={opts} onReady={handleOnReady} />
+      <Video />
 
     </>
   )
