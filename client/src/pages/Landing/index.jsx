fe75f5b52187febfeadb69ff6e3c12dfb5f06fbd
MakoskiVictor <makoski.ed@gmail.com>
2023-08-14 17:35:30 -0300

    add video

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,10 +1,20 @@
-import { Loader } from '../../components/Loader'
+import YouTube from 'react-youtube'
 
 export default function Landing () {
+  const handleOnReady = (event) => {
+    event.target.playVideo() // Reproduce automáticamente el video
+  }
+
+  const opts = {
+    height: '780',
+    width: '1280'
+  }
+
   return (
     <>
       <h1>Landing Page</h1>
-      <Loader/>
+      <YouTube videoId='5UnjrG_N8hU' opts={opts} onReady={handleOnReady} />
+
     </>
   )
 }
