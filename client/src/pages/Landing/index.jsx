44c41cd85a258ee9b49154bda75d70efae91cd22
MakoskiVictor <makoski.ed@gmail.com>
2023-08-15 12:32:32 -0300

    add img and button

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,11 +1,21 @@
-import { Video } from '../../components/Video'
+import { Video } from './Video'
+import styles from './Landing.module.css'
+/* import starWars from '../../assets/Images/starWars.svg' */
 
 export default function Landing () {
   return (
-    <>
-      <h1>Landing Page</h1>
-      <Video />
+    <div className={styles.landing} >
 
-    </>
+      <div className={styles.imgTitleContainer} >
+
+        <div className={styles.startBtn} > <div className={styles.start} >START</div> </div>
+      </div>
+        <img src="https://res.cloudinary.com/dl9pbe0eu/image/upload/v1692100179/1310579_vekigt.webp" alt="Star Wars Img" className={styles.wallpaper} />
+
+      <div className={styles.bottom} >
+        <Video />
+      </div>
+
+    </div>
   )
 }
