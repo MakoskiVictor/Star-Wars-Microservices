0f1dcdae972b219f15edb5aa3502a40b4bcbd699
MakoskiVictor <makoski.ed@gmail.com>
2023-08-25 17:04:35 -0300

    add media and link

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,13 +1,22 @@
 import trailer from '../../assets/Video/trailer.webm'
 import styles from './Landing.module.css'
+import cellWars from '../../assets/Images/cellWars.webp'
 
 export default function Landing () {
   return (
     <main className={styles.landing}>
       <section className={styles.section} >
-        <h1>Star Wars Microservices</h1>
+        <div className={styles.title} >
+          <h1>Star Wars Microservices</h1>
+        </div>
         <div className={styles.videoContainer}>
           <video src={trailer} autoPlay muted loop className={styles.video}></video>
+        </div>
+        {/* <div className={styles.celImgContainer}> */}
+          <img src={cellWars} alt="Star Wars Img" className={styles.celImg} />
+        {/* </div> */}
+        <div className={styles.start} >
+          <a href='/characters' className={styles.link} >Characters</a>
         </div>
       </section>
     </main>
