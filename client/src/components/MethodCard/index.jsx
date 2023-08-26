0015aca8a77778582b65423f9b6f3a7b99d0e58d
MakoskiVictor <makoski.ed@gmail.com>
2023-08-26 12:59:42 -0300

    add styles to component

--- client/src/components/MethodCard/index.jsx
+++ client/src/components/MethodCard/index.jsx
@@ -1,28 +1,23 @@
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { CopyIcon } from '../Icons'
-import { getLinkById, EXAMPLE_GET_ALL_CHARACTERS } from '../../utils/constants'
+import { getLinkById } from '../../utils/constants'
+import style from './MethodCard.module.css'
 
 export default function MethodCard ({ id }) {
   const getLink = getLinkById(id)
-
-  const exampleRequest = JSON.stringify(EXAMPLE_GET_ALL_CHARACTERS)
 
   const handleClipboardLink = () => {
     navigator.clipboard.writeText(getLink).catch(() => {})
   }
 
-  /* const handleClipboardExample = () => {
-    navigator.clipboard.writeText(getLink).catch(() => {})
-  } */
-
   return (
     <div>
     <h3> {id} </h3>
-    <div>
-      <div>
-      <div>
-        URL
+    <div className={style.container} >
+      <div className={style.containerHead} >
+      <div className={style.uri} >
+        URI
       </div>
       <div>
         <button
@@ -31,27 +26,11 @@ export default function MethodCard ({ id }) {
         </button>
       </div>
       </div>
-      <SyntaxHighlighter language="jsx" style={okaidia}>
+      <SyntaxHighlighter language="jsx" style={okaidia} className={style.syntax} >
         {getLink}
       </SyntaxHighlighter>
     </div>
 
-    <div>
-      <div>
-      <div>
-        Example Response
-      </div>
-      <div>
-        {/* <button
-        onClick={handleClipboardExample}>
-          <CopyIcon />
-        </button> */}
-      </div>
-      </div>
-      <SyntaxHighlighter language="javascript" style={okaidia}>
-        {exampleRequest}
-      </SyntaxHighlighter>
-    </div>
   </div>
 
   )
