d719fa79c67fd2e8055b178c3a26c5bf248b4cc6
MakoskiVictor <makoski.ed@gmail.com>
2023-08-26 11:21:33 -0300

    use MethodCatd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,44 @@
 import './App.css'
-import { Routes, BrowserRouter, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Home from './pages/Home'
-import Characters from './pages/Characters'
-/* import Films from './pages/Films'
-import Planets from './pages/Planets' */
-import NotFound from './pages/NotFound'
-import DetailsCharacter from './pages/DetailsCharacter'
-/* import DetailsFilm from './pages/DetailsFilm'
-import DetailsPlanet from './pages/DetailsPlanet' */
+import MethodCard from './components/MethodCard'
 
 function App () {
   return (
-    <BrowserRouter>
-    <Routes>
+    <main>
+      <h1>Star Wars Microservices</h1>
+      <section>
+        <p>All Star Wars data you´ll never need in one place, easily accsessible through a modern REST Api with Microservices Architecture!</p>
+      </section>
+      <section>
 
-        <Route path='/' element={<Landing/>} />
+          <h2>What are the Api Methods?</h2>
+          <ol>
+            <li>Get All Characters</li>
+            <li>Get All Planets</li>
+            <li>Get All Films</li>
+            <li>Get Character by ID</li>
+            <li>Get Planet by ID</li>
+            <li>Get Film by ID</li>
+            <li>Post Planet</li>
+            <li>Post Film</li>
+            <li>Update Character</li>
+            <li>Update Planet</li>
+            <li>Update Film</li>
+          </ol>
+          </section>
 
-        <Route path='/home' element={<Home/>} />
+          <MethodCard id='Get All Characters' />
+          <MethodCard id='Get All Planets' />
+          <MethodCard id='Get All Films' />
+          <MethodCard id='Get Character by ID' />
+          <MethodCard id='Get Planet by ID' />
+          <MethodCard id='Get Film by ID' />
+          <MethodCard id='Post Planet' />
+          <MethodCard id='Post Film' />
+          <MethodCard id='Update Character' />
+          <MethodCard id='Update Planet' />
+          <MethodCard id='Update Film' />
 
-        <Route path='/characters' element={<Characters/>} />
-
-        <Route path='/characters/:id' element={<DetailsCharacter/>} />
-
-        {/* <Route path='/films' element={<Films/>} />
-
-        <Route path='/films/:id' element={<DetailsFilm/>} />
-
-        <Route path='/planets' element={<Planets/>} />
-
-        <Route path='/planets/:id' element={<DetailsPlanet/>} /> */}
-
-        <Route path='*' element={<NotFound/>} />
-
-    </Routes>
-    </BrowserRouter>
+    </main>
   )
 }
 
