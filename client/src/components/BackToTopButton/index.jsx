34f528d4ad583ce81d54d3979939f7fe12e79810
MakoskiVictor <makoski.ed@gmail.com>
2023-09-02 19:36:28 -0300

    add eventListener

--- client/src/components/BackToTopButton/index.jsx
+++ client/src/components/BackToTopButton/index.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react'
+import { ToTopIcon } from '../Icons/index'
+import style from './BackToTopButton.module.css'
+
 export default function BackToTopButton () {
+  const [backButton, setBackButton] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setBackButton(true)
+      } else {
+        setBackButton(false)
+      }
+    })
+  }, [])
   return (
-    <div>Button</div>
+
+      <a href="#title" className={backButton ? style.toTop2 : style.toTop1} >
+        <div className={backButton ? style.icon2 : style.icon}>
+          <ToTopIcon />
+        </div>
+      </a>
+
   )
 }
